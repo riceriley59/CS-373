8cd276c2223c386ff1fcc30bb6a34cd2df217758
Riley Rice <riceriley59@gmail.com>
2025-01-21 15:25:06 -0800

    Finished port scanner

--- badmap/src/main.rs
+++ badmap/src/main.rs
@@ -2,15 +2,17 @@
 use clap::Parser;
 use dns_lookup::lookup_host;
 
-// tokio to asynchronously scan ports
+// tokio to asynchronously scan ports, write to files, etc.
 use tokio::task;
 use tokio::net::TcpStream;
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
+use tokio::sync::Semaphore;
 
 // other imports
 use std::process::exit;
 use std::net::IpAddr;
+use std::sync::Arc;
 
 // define CLI args using clap
 #[derive(Parser, Debug)]
@@ -24,6 +26,15 @@ struct Args {
 
     #[arg(short, long, default_value = "output.txt")]
     output_filename: String,
+}
+
+fn get_service_by_port(port: u16) -> Option<&'static str> {
+    match port {
+        22 => Some("SSH"),
+        80 => Some("HTTP"),
+        443 => Some("HTTPS"),
+        _ => None,
+    }
 }
 
 // gets and validates IP address for host that we want to scan
@@ -84,13 +95,32 @@ async fn scan_ports(target: String) -> Vec<u16> {
     let mut tasks = Vec::new();
     let mut open_ports = Vec::new();
 
+    // create sempahore with 1000 available spots
+    // This makes it so that only 1000 threads can
+    // run at the same time
+    let semaphore = Arc::new(Semaphore::new(1000));
+
     for port in 1..=65535 {
-        tasks.push(tokio::spawn(scan_port(target.clone(), port)));
+        // have to clone so each thread doesn't own
+        // target or the semaphore
+        let semaphore = Arc::clone(&semaphore);
+        let target_copy = target.clone();
+
+        tasks.push(tokio::spawn(async move {
+            let permit = semaphore.acquire().await.unwrap();
+            let result = scan_port(target_copy, port).await;
+            drop(permit);
+            result
+        }));
     }
+
+    println!("PORT  STATE  SERVICE\n");
 
     for task in tasks {
         if let Some(port) = task.await.unwrap() {
-            println!("port {} is open", port);
+            let service = get_service_by_port(port).unwrap_or("<unknown>");
+
+            println!("{}  open  {}", port, service);
             open_ports.push(port);
         }
     }
@@ -99,17 +129,39 @@ async fn scan_ports(target: String) -> Vec<u16> {
 }
 
 // Writes the open ports to a file
-async fn write_output_file(open_ports: Vec<u16>, output_filename: &str) {
-    match File::create(output_filename).await {
+async fn write_output_file(open_ports: Vec<u16>, target: String, args: &Args) {
+    match File::create(args.output_filename.clone()).await {
         Ok(mut file) => {
+            let start_line = format!("Starting BadMap {}\n", option_env!("CARGO_PKG_VERSION").unwrap_or("<uknown>"));
+            if let Err(err) = file.write_all(start_line.as_bytes()).await {
+                eprintln!("Failed to write to file: {}", err);
+            }
+
+            let target_line: String;
+            if !args.domain.is_empty() {
+                target_line = format!("BadMap Scan Report for ({}) {}\n\n", args.domain, target);
+            } else {
+                target_line = format!("BadMap Scan Report for {}\n\n", target);
+            }
+            if let Err(err) = file.write_all(target_line.as_bytes()).await {
+                eprintln!("Failed to write to file: {}", err);
+            }
+
+            if let Err(err) = file.write_all(format!("PORT  STATE  SERVICE\n\n").as_bytes()).await {
+                eprintln!("Failed to write to file: {}", err);
+            }
+
             for port in open_ports {
-                let line = format!("Port {} is open\n", port);
+                // Get the service name or "unknown" if not found
+                let service = get_service_by_port(port).unwrap_or("<unknown>");
+
+                let line = format!("{}  open  {}\n", port, service);
                 if let Err(err) = file.write_all(line.as_bytes()).await {
                     eprintln!("Failed to write to file: {}", err);
                 }
             }
 
-            println!("Results saved to {}", output_filename);
+            println!("\nResults saved to {}", args.output_filename);
         }
         Err(err) => {
             eprintln!("Failed to create output file: {}", err);
@@ -121,12 +173,16 @@ async fn write_output_file(open_ports: Vec<u16>, output_filename: &str) {
 #[tokio::main]
 async fn main() {
     let args: Args = Args::parse();
+    println!("Starting BadMap {}", option_env!("CARGO_PKG_VERSION").unwrap_or("<uknown>"));
+
     let target: String = get_target(&args).await;
+    if !args.domain.is_empty() {
+        println!("BadMap Scan Report for ({}) {}\n", args.domain, target);
+    } else {
+        println!("BadMap Scan Report for {}\n", target);
+    }
 
-    println!("target: {}", target); 
+    let open_ports: Vec<u16> = scan_ports(target.clone()).await;
 
-    let open_ports: Vec<u16> = scan_ports(target).await;
-
-    write_output_file(open_ports, &args.output_filename).await;
+    write_output_file(open_ports, target.clone(), &args).await;
 }
-
